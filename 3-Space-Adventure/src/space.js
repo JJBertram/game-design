28ea4dca7c5062a885135c00649ef8df10d07a70
James Bertram <JamesBertram@tutanota.com>
2015-07-28 11:39:58 -0700

    visual updates, game logic and functions added

--- 3-Space-Adventure/src/space.js
+++ 3-Space-Adventure/src/space.js
@@ -4,36 +4,41 @@
 //html reference to stage and output
 var stage = document.querySelector("#stage");
 var output = document.querySelector("#output");
+var fuel = document.querySelector("#fuel");
+var food = document.querySelector("#grub");
+var credits = document.querySelector("#credits");
+var xp = document.querySelector("#xp");
+window.addEventListener("keydown", keydownHandler, false);
 //2-D 'map' array
 var map =
 [
-	[0, 1, 0, 0, 0, 4],
-	[0, 0, 2, 3, 0, 0],
-	[1, 0, 0, 0, 0, 1],
+	[2, 0, 0, 1, 0, 7],
+	[0, 6, 0, 0, 1, 0],
+	[1, 0, 0, 5, 0, 1],
+	[0, 3, 0, 1, 0, 0],
 	[0, 0, 0, 0, 0, 0],
-	[0, 2, 0, 0, 1, 0],
-	[0, 0, 2, 0, 0, 3]
+	[8, 0, 6, 0, 0, 4],
 ];
 //game objects array - things that move
 var gameObjects =
 [
-	[0, 1, 0, 0, 0, 4],
-	[0, 0, 2, 3, 0, 0],
-	[1, 0, 0, 0, 0, 1],
 	[0, 0, 0, 0, 0, 0],
-	[0, 2, 0, 0, 1, 0],
-	[5, 0, 2, 0, 0, 3]
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0],
+	[8, 0, 0, 0, 0, 0],
 ];
 //map key
 var SPACE = 0;
 var PIRATE = 1;
 var PLANET_PURPLE = 2;
-//var PLANET_RED =3;
-//var PLANET_BROWN = 4;
-//var PLANET_GREEN = 5;
-var STATION = 3;
-var VORTEX = 4; //destination
-var SHIP = 5; //player
+var PLANET_RED =3;
+var PLANET_BROWN = 4;
+var PLANET_GREEN = 5;
+var STATION = 6;
+var VORTEX = 7; //destination
+var SHIP = 8; //player
 //cell size
 var SIZE = 64;
 var ROWS = map.length;
@@ -41,6 +46,27 @@ var COLUMNS = map[0].length;
 //player ship loc
 var shipRow;
 var shipCol;
+//keydown values
+var UP = 38;
+var DOWN = 40;
+var LEFT = 37;
+var RIGHT = 39;
+//game variables
+var fuelAmount = 10;
+var creditAmount = 10;
+var foodAmount = 10;
+var xpAmount = 0;
+//output text
+var gameMessage = "Use arrow keys to find your way to the vortex<br>"
+	+ "You will need fuel, credits, and food to explore and fight";
+output.innerHTML = gameMessage;
+fuel.innerHTML = fuelAmount;
+credits.innerHTML = creditAmount;
+food.innerHTML = foodAmount;
+xp.innerHTML = xpAmount;
+
+
+//loop to get initial player ship position
 for(var row = 0; row < ROWS; row++) 
 {	
   for(var col = 0; col < COLUMNS; col++) 
@@ -48,11 +74,203 @@ for(var row = 0; row < ROWS; row++)
     if(gameObjects[row][col] === SHIP)
     { 
       shipRow = row;
-      shipColumn = col;
+      shipCol = col;
     }
   }
 }
+
 render();
+function keydownHandler(event)
+{
+	switch(event.keyCode)
+	{
+		case UP:
+			if(shipRow > 0)
+			{
+				gameObjects[shipRow][shipCol] = 0; //clear current cell
+				shipRow --; //change row value
+				gameObjects[shipRow][shipCol] = SHIP; //move ship to new position
+				console.log("up: " + UP);
+			}
+			break;
+		case DOWN:
+			if(shipRow < ROWS - 1)
+			{
+				gameObjects[shipRow][shipCol] = 0;
+				shipRow ++;
+				gameObjects[shipRow][shipCol] = SHIP;
+			}
+			break;
+		case LEFT:
+			if(shipCol > 0)
+			{
+				gameObjects[shipRow][shipCol] = 0;
+				shipCol --;
+				gameObjects[shipRow][shipCol] = SHIP;
+			}
+			break;
+		case RIGHT:
+			if(shipCol < COLUMNS - 1)
+			{
+				gameObjects[shipRow][shipCol] = 0;
+				shipCol ++;
+				gameObjects[shipRow][shipCol] = SHIP;
+			}
+			break;
+	} //end keydown switch
+	switch(map[shipRow][shipCol])
+	{
+		case SPACE:
+			gameMessage = "You drift through silent space.";
+			break;
+		case PIRATE:
+			gameMessage = "You've encountered space pirates! ayyyyy!!";
+			fight();
+			break;
+		case PLANET_PURPLE:
+			gameMessage = "An agricultural farm world for this system.";
+			trade();
+			break;
+		case PLANET_BROWN:
+			gameMessage = "An industrial mining colony remains here.";
+			trade();
+			break;
+		case PLANET_GREEN:
+			gameMessage = "An agricultural farm world for this system.";
+			trade();
+			break;
+		case PLANET_RED:
+			gameMessage = "You've entered a lawless domain, prepare to fight!";
+			fight();
+			break;
+		case STATION:
+			gameMessage = "A supply station, time to fill up!";
+			trade();
+			break;
+		case VORTEX:
+			gameMessage = "You've made it to the vortex!";
+			endGame();
+			break;
+	}//end location info switch
+	//update ships supplies
+	fuelAmount--;
+	foodAmount--;
+	if(fuelAmount <= 0 || foodAmount <= 0)
+	{
+		endGame();
+	}
+}
+function fight()
+{
+	var shipStrength = Math.ceil((foodAmount+creditAmount)/2); 
+	var pirateStrength = Math.ceil(Math.random()*shipStrength *2);
+	if(pirateStrength > shipStrength) 
+	{
+		var creditsLost = Math.round(pirateStrength/2);
+		creditAmount -= creditsLost;
+		xpAmount += 1;
+		gameMessage += "<br>You fight and <strong><em>LOSE.</em></strong><br>"
+		+ "<span style= \"float:left; color: red;\">credits lost: </span><span id = \"youLose\">-" 
+		+ creditsLost + " </span></strong><br>"
+		+ "experience gained: <span id = \"youWin\">+" + 1
+		+ "</span><br>Your ship's strength: <span>" 
+		+ shipStrength + "</span><br>The pirate's strength: <span>" + pirateStrength + "</span>";
+	}
+	else
+	{
+		var pirateCredits = Math.round(pirateStrength/2);
+		creditAmount += pirateCredits;
+		xpAmount += 2;
+		gameMessage += "<br>You fight and <strong><em>WIN.</em></strong><br>"
+		+"<span style= \"float:left;\">credits gained: </span><span id = \"youWin\">+" 
+		+ pirateCredits + " </span></strong><br>"
+		+ "experience gained: <span id = \"youWin\">+" + 2
+		+ "</span><br>Your ship's strength: <span>" + shipStrength 
+		+ "</span><br>pirate's strength: <span>" + pirateStrength +"</span>";
+	}
+}//end fight function
+function trade()
+{
+	var planetResources = xpAmount + creditAmount;
+	var cost = Math.ceil(Math.random() * planetResources);
+	if(creditAmount > cost)
+	{
+		switch(map[shipRow][shipCol])
+		{
+			case PLANET_PURPLE:
+				foodAmount += cost;
+				xpAmount += 2;
+				gameMessage += " Nice trade! You've gained food and experience.<br>"
+				+ "<span style= \"float:left;\">food gained: </span><span id = \"youWin\">+" 
+				+ cost + " </span><br>" 
+				+ "experience gained: <span id = \"youWin\">+" + 2 + "</span>";
+				break;
+			case PLANET_RED:
+				fight();
+				break;
+			case PLANET_GREEN:
+				foodAmount += cost;
+				xpAmount += 2;
+				gameMessage += " Nice trade! You've gained food and experience.<br>"
+				+ "<span style= \"float:left;\">food gained: </span><span id = \"youWin\">+" 
+				+ cost + " </span><br>" 
+				+ "experience gained: <span id = \"youWin\">+" + 2 + "</span>";
+				break;
+			case PLANET_BROWN:
+				fuelAmount += cost;
+				xpAmount += 2;
+				gameMessage += " Nice trade! You've gained fuel and experience.<br>"
+				+ "<span style= \"float:left;\">fuel gained: </span><span id = \"youWin\">+" 
+				+ cost + " </span><br>" 
+				+ "experience gained: <span id = \"youWin\">+" + 2 + "</span>";
+				break;
+			case STATION:
+				console.log("cost: " + cost);
+				fuelAmount += cost;
+				xpAmount += 1;
+				gameMessage += " Nice trade! You've gained fuel and experience.<br>"
+				+ "<span style= \"float:left;\">fuel gained: </span><span id = \"youWin\">+" 
+				+ cost + " </span><br>" 
+				+ "experience gained: <span id = \"youWin\">+" + 1 + "</span>";
+				break;
+		} //end trade switch
+	} //end if
+	else
+	{
+		gameMessage += "<span id = \"youLose\"> We have insufficient credits to trade here..</span>"
+		+ "<br>station price: <span>" + cost
+		+ "</span><br>total credits: <span> " + creditAmount+"</span>";
+	}
+}//end trade func
+function endGame()
+{
+	if(map[shipRow][shipCol] === VORTEX)
+	{
+		var score = creditAmount+foodAmount+xpAmount+fuelAmount;
+		gameMessage += "<br>Dear god man you've done it!"
+		+ "<br><h2>Final Score: <span>" + score + "</span></h2>";		
+	}
+	else
+	{
+		if(fuelAmount <= 0)
+		{
+			gameMessage = "No..More..Fuel..The crew sit in silence, throwing nervous glances between themselves. "
+			+ "It won't be long now before hunger and desperation consume the last of you."
+			+ "<br><span id = \"youLose\">You Lose</span>";
+		}
+		else if(foodAmount <= 0)
+		{
+			gameMessage = "No..More..Food..With no fuel, what little crew left alive is too weak to seek help.."
+			+"your ship drifts forever into the void."
+			+ "<br><span id = \"youLose\">You Lose</span>";
+		}
+		else
+		{
+			gameMessage = "because aliens";
+		}
+	}//end if
+	window.removeEventListener("keydown", keydownHandler, false);
+}// end endGame func
 function render()
 {
 	//look for old child divs and remove
@@ -84,6 +302,16 @@ function render()
 				case PLANET_PURPLE:
 					cell.src=("src/images/planet-purple.png");
 					break;
+				case PLANET_RED:
+					cell.src=("src/images/planet-red.png");
+					break;
+				case PLANET_GREEN:
+					cell.src=("src/images/planet-green.png");
+					break;
+				case PLANET_BROWN:
+					cell.src=("src/images/planet-brown.png");
+					break;
+					
 				case STATION:
 					cell.src=("src/images/spacestation.png");
 					break;
@@ -94,13 +322,19 @@ function render()
 			switch(gameObjects[row][col])
 			{
 				case SHIP:
-					cell.src="src/images/ship.png";
+					cell.src=("src/images/ship.png");
 					break;
 			}
 			//position image cell
 			cell.style.top = row * SIZE + "px";
 			cell.style.left = col * SIZE + "px";
-		}
-	}
+		} //inner for loop end
+	} //outer for loop end
+	fuel.innerHTML = fuelAmount;
+	credits.innerHTML = creditAmount;
+	food.innerHTML = foodAmount;
+	xp.innerHTML = xpAmount;
+	output.innerHTML = gameMessage;
 }
+
 }());//end main function wrapper
